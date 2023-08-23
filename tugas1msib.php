36a7371c35e4f2ecc572e125a6720e4eaac1ba66
Maydupi Astri Turnip <139747765+astry27@users.noreply.github.com>
2023-08-24 00:58:08 +0800

    Add files via upload

---
+++ tugas1msib.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            font-family: 'Times New Roman', Times, serif, sans-serif;
+            background-image:url("https://images.freecreatives.com/wp-content/uploads/2016/03/Smooth-Blue-Grunge-Background.jpg");
+            background-size: cover;
+            background-position: center;
+        }
+        .container {
+            max-width: 500px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: white;
+            box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
+            border-radius: 5px;
+        }
+        h1 {
+            text-align: center;
+            color: #333;
+        }
+        p {
+            color: #333;
+        }
+    </style>
+    <title>Data Diri</title>
+</head>
+<body>
+    <div class="container">
+        <table cellpadding="10px">
+        <h1>Data Diri</h1>
+        <h4><center>Peserta Studi Independent (MSIB) Batch 5 GITS Indonesia Tahun 2023</center></h4><HR>
+        <tr>
+        <td>Nama </td>      
+        <td>: Maydupi Astri Turnip</td> 
+        <td rowspan ="5"><img src ="Maydupi.png" height="180px" width="135"></td>  
+        </tr>
+        <tr>
+        <td>Jurusan </td>         
+        <td>: Teknik Informatika</td> 
+        </tr>  
+        <tr>
+        <td>Semester </td>         
+        <td>: 5 (Lima)</td> 
+        </tr>
+        <tr>
+        <td>Asal Institusi </td>         
+        <td>: Universitas Negeri Manado</td>   
+        </tr>
+        <tr>
+        <td>Alamat Domisili </td>         
+        <td>: Sulawesi Utara</td>   
+        </tr>
+        </table>
+    </div>
+</body>
+</html>
